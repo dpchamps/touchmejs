2e12aadbd3318ef9474a3473bfe0689fb7012398
gasolinewaltz <gasolinewaltz@gmail.com>
2016-01-03 16:56:34 -0800

    we have pinch functionality

--- dist/touchme.js
+++ dist/touchme.js
@@ -42,12 +42,14 @@ var touchme = function(args) {
         oldX, oldY,
         holdElement, isHolding,
         originalX, originalY, //for elements that are being held
-        holdInterval, lastX, lastY; //cursor tracking while holding
+        holdInterval, lastX, lastY, //cursor tracking while holding
+        initialPinch;
 
     //where args is an object that can replace any of the default arguments
     var defaults = {
         swipeThreshold: 80,
         tapThreshold: 150,
+        pinchResolution: 15,
         holdThreshold: 550,
         precision: 45,
         onlyTouch: false,
@@ -94,6 +96,62 @@ var touchme = function(args) {
             return event;
         }
     };
+    var isPinch = function(e){
+      var pinching = false;
+      if( e.targetTouches && e.targetTouches[0] && e.targetTouches[1]){
+        pinching = true;
+      }
+
+      return pinching;
+    };
+    var getTouchPoints = function(event){
+      var touchPoints = {},
+          touches = event.targetTouches;
+      for(var i = 0; i < touches.length; i++){
+          var keyName = "touch"+i;
+          touchPoints[keyName] = {
+            x : touches[i].pageX,
+            y : touches[i].pageY,
+            id : touches[i].identifier
+          };
+      }
+      return touchPoints;
+    };
+
+    var getMidPoint = function(points){
+      var x1 = points['touch0'].x,
+          x2 = points['touch1'].x,
+          y1 = points['touch0'].y,
+          y2 = points['touch1'].y,
+          midpoint;
+
+      midpoint = {
+        x : (x1+x2)/2,
+        y : (y1+y2)/2
+      };
+
+      return midpoint;
+    };
+    var getDistance = function(x1,x2,y1,y2){
+      //it's a little dense, but it's the fucking distance formula.
+      return Math.sqrt( Math.pow( (x2-x1) , 2) + Math.pow( (y2-y1), 2) );
+    };
+    var isPinchRes = function(firstPinch, newPinch){
+      var touch1Dist = getDistance( firstPinch['touch0'].x,
+                                    newPinch['touch0'].x,
+                                    firstPinch['touch0'].y,
+                                    newPinch['touch0'].y),
+          touch2Dist = getDistance( firstPinch['touch1'].x,
+                                    newPinch['touch1'].x,
+                                    firstPinch['touch1'].y,
+                                    newPinch['touch1'].y),
+          isGreater = false;
+
+      if(touch1Dist > defaults.pinchResolution || touch2Dist > defaults.pinchResolution){
+        isGreater = true;
+      }
+      return isGreater;
+    };
     /*
         triggerEvent()
             creates a custom event, and passes any members in the data object to the event
@@ -124,7 +182,7 @@ var touchme = function(args) {
         touchDevice = true;
     }
 
-    //tap, dbltap, ntap, hold
+    //tap, dbltap, ntap, hold, initialPinch
     setListener(document, touchDevice ? 'mousedown touchstart' : 'mousedown', function(e){
         touchStart = true;
         tapNumber += 1;
@@ -189,10 +247,18 @@ var touchme = function(args) {
                 }
             }, defaults.holdThreshold);
         }
-
+        //here we set the initial pinch to compare distances
+        if(isPinch(e)){
+          initialPinch =  getTouchPoints(e);
+          initialPinch['distance'] = getDistance( initialPinch['touch0'].x,
+                                                initialPinch['touch1'].x,
+                                                initialPinch['touch0'].y,
+                                                initialPinch['touch1'].x);
+        }
     });
 
     //track the movement / drag
+    // detect pinch gesture
     setListener(document, touchDevice ? 'mousemove touchmove' : 'mousemove', function(e){
         var pointer = getPointer(e);
         currentX = pointer.pageX;
@@ -204,6 +270,26 @@ var touchme = function(args) {
             y: currentY
           });
         }
+        if(isPinch(e)){
+          /*
+            returns array of objects, with props: x, y, id
+           */
+          var pinch = {};
+          pinch.touchPoints = getTouchPoints(e);
+          pinch.distance = getDistance( pinch.touchPoints['touch0'].x,
+                                  pinch.touchPoints['touch1'].x,
+                                  pinch.touchPoints['touch0'].y,
+                                  pinch.touchPoints['touch1'].y);
+
+          pinch.midPoint = getMidPoint(pinch.touchPoints);
+          pinch.initialPinch = initialPinch;
+
+          if( isPinchRes(pinch.initialPinch, pinch.touchPoints) ){
+            triggerEvent(e.target, 'pinch', pinch);
+            initialPinch = pinch.touchPoints;
+            initialPinch['distance'] = pinch.distance;
+          }
+        }
     });
 
     /*
@@ -214,7 +300,9 @@ var touchme = function(args) {
      */
     setListener(document, touchDevice ? 'mouseup touchend' : 'mouseup', function(){
         touchStart = false;
-
+        if(initialPinch){
+          initialPinch = undefined;
+        }
         //if the user was holding something...
         if(isHolding){
             isHolding = false;
